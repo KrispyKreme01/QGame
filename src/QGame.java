a2af790fe9ba385c619a0067e0d5d4855a2880a5
KrispyKreme01 <krmalatrash@gmail.com>
2017-05-19 20:34:39 -0400

    +used for loop to check through question string to make sure question and answer were equal
    +added score counter
    
    +made 3 methods that checked if you got the right or wrong answer

--- src/QGame.java
+++ src/QGame.java
@@ -1,12 +1,30 @@
 import java.util.Scanner;
 public class QGame {
-	public static String eRiddles[] = new String[5];
-	public static String mRiddles[] = new String[5];
-	public static String hRiddles[] = new String[5];
-	public static Scanner sc = new Scanner(System.in);
+	public static boolean chkAnswer = false;
+	public static String[] eRiddles = new String[] {"What doesn't get any wetter, no matter how much rain falls on it?", "Feed me and I live, yet give me a drink and I die. What am I?", 
+		                                             "What is easy to get into, but hard to get out of?", "What belongs to you but others use it more than you do?",
+		                                             "Fashions change but what can a person wear that is never out of style?", "What kind of key do you use on Thanksgiving?"};
+	public static String mRiddles[] = new String[] {"What did the thief get for stealing the calendar?",
+													"What is big and yellow and comes in the morning, to brighten mom's day?",
+													"I am always in front and never behind. What am I?",
+													"You will always find me in the past. I can be created in the present, But the future can never taint me. What am I?",
+													"What has many keys but cannot open a single lock",
+													"What kind of dog keeps the best time?"};
+	public static String hRiddles[] = new String[] {"I pass before the sun, yet make no shadow. What am I?",
+													"I can fly but have no wings. I can cry but I have no eyes. Wherever I go, darkness follows me.",
+													"The more you take, the more you leave behind. What am I?",
+													"You throw away the outside and cook the inside. Then you eat the outside and throw away the inside. What did you eat?",
+													"Whats a seven letter word containing thousands of letters",
+													"Which word does not belong in the following list: Stop cop mop chop prop shop or crop?"};
+	
+	public static String eAnswer[] = new String[] {"water", "fire", "trouble", "name", "smile", "turkey"};
+	public static String mAnswer[] = new String[] {"12 months", "school bus", "future", "sistory", "keyboard", "watchdog"} ;
+	public static String hAnswer[] = new String[] {"wind", "clouds", "footsteps", "corn", "mailbox"};
+	public static String userAnswer[] = new String[5];
 	public static int dCounter = 0;
+	public static int score = 0;
+	public static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		System.out.println("welcome message goes here\n\n\n");
 		chooseDiff();
@@ -23,7 +41,7 @@ public class QGame {
 		default:
 			System.out.println("error");
 		}
-		
+		System.out.println("congrats, youre done: " + score);
 		
 	}
 	
@@ -61,14 +79,46 @@ public class QGame {
 	}
 	public static void diffE() {
 		System.out.println("you have chosen easy");
-		
+		for (int i = 1; i < 6; i++) {
+			System.out.println("here is riddle #"+ i + " " + eRiddles[i-1] + "\n\n what is your answer?");
+			userAnswer[i-1] = sc.nextLine().toLowerCase();
+			if(userAnswer[i-1].contains(eAnswer[i-1])) {
+				System.out.println("CORRECT, + 1 POINT");
+				score++;
+			}
+			else {
+				System.out.println("not correct, no points awarded");
+			}
+		}
 	}
 	public static void diffM() {
 		System.out.println("you have chosen medium");
-		
+		for (int i = 1; i < 6; i++) {
+			System.out.println("here is riddle #"+ i + mRiddles[i-1] + "\n\n what is your answer?");
+			userAnswer[i-1] = sc.nextLine().toLowerCase();
+			if(userAnswer[i-1].contains(mAnswer[i-1])) {
+				System.out.println("CORRECT, + 1 POINT");
+				score++;
+			}
+			else {
+				System.out.println("not correct, no points awarded");
+			}
+		}
 	}
 	public static void diffH() {
 		System.out.println("you have chosen hard");
+		System.out.println("you have chosen medium");
+		for (int i = 1; i < 6; i++) {
+			System.out.println("here is riddle #"+ i +hRiddles[i-1] + "\n\n what is your answer?");
+			userAnswer[i-1] = sc.nextLine().toLowerCase();
+			if(userAnswer[i-1].contains(hAnswer[-1])) {
+				System.out.println("CORRECT, + 1 POINT");
+				score++;
+			}
+			else {
+				System.out.println("not correct, no points awarded");
+			}
+		}
 		
 	}
 
